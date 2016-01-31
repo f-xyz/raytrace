a3393ae74ac6f0d11475cb7eaba6e30e17081c34
f-xyz <andrey.lischina@gmail.com>
2016-01-31 23:14:32 +0200

    .

--- shaders/raymarching.fragment.glsl
+++ shaders/raymarching.fragment.glsl
@@ -72,25 +72,34 @@ intersection world(vec3 v) {
 //    );
 
     intersection sphere1;
-    sphere1.path = sphere(v + vec3(0., 0., 6.), 5.);
+    sphere1.path = sphere(v, 5.);
     sphere1.material = 0.;
     sphere1.reflectivity = 1.;
 
 //    return sphere1;
 
-    intersection sphere2;
-    sphere2.path = sphere(v + vec3(0., 0., -6.), 5.);
-    sphere2.material = 0.;
-    sphere2.reflectivity = 1.;
+//    intersection sphere2;
+//    sphere2.path = sphere(v + vec3(0., 0., -6.), 5.);
+//    sphere2.material = 0.;
+//    sphere2.reflectivity = 1.;
 
-    intersection plane;
-    plane.path = v.y + 10.;
-    plane.material = 1.;
-    plane.reflectivity = 1.;
+    intersection plane1;
+    plane1.path = v.y + 10.;
+    plane1.material = 1.;
+    plane1.reflectivity = 1.;
 
-//    return plane;
+//    intersection universe;
+//    universe.path = signedBox(v, vec3(4.));
+//    universe.material = 2.;
+//    universe.reflectivity = 1.;
 
-    return join(sphere1, join(sphere2, plane));
+//    return join(universe, sphere1);
+
+//    return plane1;
+
+//    return join(sphere1, join(sphere2, join(plane1, universe)));
+
+    return join(plane1, sphere1);
 }
 
 intersection trace(vec3 ro, vec3 rd, float offset) {
@@ -141,28 +150,43 @@ float getAmbientOcclusion(vec3 v, vec3 normal) {
 
 vec4 getMaterial(intersection w, vec4 light) {
     vec4 color = vec4(1., 1., 1., 1.);
+
     if (w.material == 0.) {
-        color = light * vec4(0.4, 0.6, 0.1, 1.);
+
+        color = light * vec4(.4, .6, .1, 1.);
+
     } else if (w.material == 1.) {
-        if (fract(w.voxel.x/10. - 0.5) < 0.1
-        ||  fract(w.voxel.z/10. - 0.5) < 0.1) {
+
+        if (fract(w.voxel.x/10. - .5) < 0.1
+        ||  fract(w.voxel.z/10. - .5) < 0.1) {
             color = vec4(.8, .6, .4, 1.);
         } else {
             color = vec4(.0, .0, .0, 1.);
         }
+
+    } else if (w.material == 2.) {
+
+        if (fract(w.voxel.x - .5) < 0.05
+        ||  fract(w.voxel.z - .5) < 0.05) {
+            color = vec4(.8, .6, .4, 1.);
+        } else {
+            color = vec4(.0, .0, .0, 1.);
+        }
+
     }
+
     return color;
 }
 
 void main() {
 
-    float ratio = resolution.x/resolution.y;
-    vec2 uv = 2.*gl_FragCoord.xy/resolution - 1.;
+    float ratio = resolution.x / resolution.y;
+    vec2 uv = 2. * gl_FragCoord.xy / resolution - 1.;
 
     float eyeDist = 10.0;
 
     vec3 up     = vec3(0.0, 1.0, 0.0);
-    vec3 eye    = vec3(0.0, 0.0, eyeDist);
+    vec3 eye    = vec3(0.0, 0.0, 10.0);
     vec3 lookAt = vec3(0.0, 0.0, 0.0);
 
     // camera path
@@ -170,6 +194,11 @@ void main() {
     eye.x = amp*cos(time*0.3);
     eye.z = amp*sin(time*0.1);
     eye.y = amp*cos(time/10.);
+
+//    float amp = eyeDist;
+//    lookAt.x = amp*cos(time*0.3);
+//    lookAt.z = amp*sin(time*0.1);
+//    lookAt.y = amp*cos(time/10.);
 
     // mouse
 //    lookAt.x = eyeDist*sin(mouse.x*pi);
@@ -190,7 +219,8 @@ void main() {
     for (float i = 0.; i < REFLECTIONS; ++i) {
         intersection w = trace(ro, rd, 0.);
 
-//        if (w.path < 0.) break; // ???
+        if (w.path < 0.) break;
+        if (w.path > MAX_PATH) break;
 
         vec3 v = ro + rd*w.path;
         vec3 normal = getNormal(v);
@@ -199,18 +229,18 @@ void main() {
         vec4 dirLight = getDirLight(v, normal, vec4(1,1,1,1), vec4(1,1,1,1), lightPos);
 
 //        float ambientOcclusion = getAmbientOcclusion(v, normal);
-//        vec4 ambientLight = vec4(0.0/* - ambientOcclusion*/);
+//        vec4 ambientLight = vec4(0.3/* - ambientOcclusion*/);
 
-        float reflectivity = i == 0. ? .5 : .0;//(REFLECTIONS - i) / REFLECTIONS;
+        float reflectivity = (REFLECTIONS - i) / REFLECTIONS;
         vec4 color = getMaterial(w, dirLight);
-//        vec4 color = /*w.path * */ditLight * vec4(0.4, 0.6, 0.1, 1.);
+//        vec4 color = .1 * w.path * /*dirLight **/ vec4(1.);
 //        color += ambientLight;
-        gl_FragColor += reflectivity * color;
+//        gl_FragColor += reflectivity * color;
 //        gl_FragColor.g = w.path/100.;
-//        gl_FragColor = color;
+        gl_FragColor += color;
 
         // fog
-        vec4 bg = vec4(.3, .3, .3, 1.);
+//        vec4 bg = vec4(.1, .5, .8, 1.);
 //        gl_FragColor = mix(gl_FragColor, bg, smoothstep(0., MAX_PATH, w.path));
 //        gl_FragColor = mix(gl_FragColor, bg, 0.5);
 
@@ -218,8 +248,9 @@ void main() {
 
 //        break;
 
-//        if (w.path > 20.) {
-//            gl_FragColor.r = 1.;
+//        if (w.path > 100.) {
+//            gl_FragColor = vec4(0.);
+//            break;
 //        };
 
         rd = normalize(reflect(rd, normal));
@@ -227,5 +258,4 @@ void main() {
     }
 
 //    gl_FragColor *= 0.001;
-
 }
