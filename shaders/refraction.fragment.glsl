2233462d588093a451804d5b85dfcfa5ef226b49
f-xyz <andrey.lischina@gmail.com>
2016-02-15 02:00:59 +0200

    .

--- shaders/refraction.fragment.glsl
+++ shaders/refraction.fragment.glsl
@@ -1,7 +1,7 @@
 #define pi           3.14159265
-#define MAX_STEPS    40.0
+#define MAX_STEPS    400.0
 #define MAX_PATH     100.0
-#define MIN_PATH     1e-2
+#define MIN_PATH     1e-3
 #define REFLECTIONS  3.0
 
 uniform float time;
@@ -93,10 +93,10 @@ intersection world(vec3 v) {
 
     //
     intersection stuff;
-    stuff.path = max(
+    stuff.path = /*max(
         signedBox(vRotated, vec3(4.)),
        -cross(vRotated, 3.0)
-    );
+    )*/sphere(v, 4.);
     stuff.material = 0.;
     stuff.reflectivity = 1.;
     stuff.opacity = 1.3;
@@ -115,7 +115,7 @@ intersection world(vec3 v) {
     // ground
     intersection plane;
     plane.path = v.y + 10.;
-    plane.material = 2.;
+    plane.material = 1.;
     plane.reflectivity = 1.;
     plane.opacity = 1.;
 
@@ -153,7 +153,7 @@ vec4 getDirLight(vec3 v, vec3 normal, vec3 pos) {
     if (trace(v, normalize(lightDir), 0.1).path < length(lightDir)) {
         return vec4(0.);
     }
-    return vec4(1.) * abs(dot(normal, normalize(lightDir)))
+    return vec4(1.) * max(0., dot(normal, normalize(lightDir)))
 //      / dot(lightDir, lightDir) // pointLight ~ 1/r^2
     ;
 }
@@ -168,7 +168,7 @@ vec4 getMaterial(intersection w, vec4 light) {
     } else if (w.material == 1.) {
 
         if (light == vec4(0.)) {
-            light = vec4(.1);
+            light = vec4(.2);
         }
 
         if (fract(w.voxel.x) < 0.2
@@ -179,6 +179,10 @@ vec4 getMaterial(intersection w, vec4 light) {
         }
 
     } else if (w.material == 2.) {
+
+        if (light == vec4(0.)) {
+            light = vec4(.2);
+        }
 
         color = light * vec4(.0, .5, .7, 1.);
 
@@ -192,16 +196,16 @@ void main() {
     float ratio = resolution.x / resolution.y;
     vec2 uv = 2. * gl_FragCoord.xy / resolution - 1.;
 
-    float eyeDist = 10.0;
+    float eyeDist = 5.0;
 
     vec3 up     = vec3(0.0, 1.0, 0.0);
-    vec3 eye    = vec3(0.0, 10.0, 10.0);
+    vec3 eye    = vec3(0.0, eyeDist, eyeDist);
     vec3 lookAt = vec3(0.0, 0.0, 0.0);
 
     // camera path
-//    eye.x = eyeDist*(sin(pi*(mouse.x/2.+.5)));
-//    eye.z = eyeDist*(cos(pi*(mouse.x/2.+.5)));
-//    eye.y = 2. + 10.*mouse.y;
+    eye.x = eyeDist*(cos(time*0.3 + 1.) + sin(pi*(mouse.x/2.+.5)));
+    eye.z = eyeDist*(sin(time*0.1 + 2.) + cos(pi*(mouse.x/2.+.5)));
+    eye.y = eyeDist*(cos(time/10. + 1.) - mouse.y);
 
     vec3 forward = normalize(lookAt - eye);
     vec3 x = normalize(cross(up, forward));
@@ -211,23 +215,26 @@ void main() {
     vec3 rd = normalize(ro - eye); // ray direction
 
     // clear
-//    vec4 bg = vec4(.0, .0, -uv.y, 1.);
-    vec4 bg = vec4(.0, .0, 1., 1.);
-    gl_FragColor = vec4(0.);
+    vec4 bg = vec4(.0, .0, .1, 1.);
+    gl_FragColor = bg;
 
     //
     for (float i = 0.; i < REFLECTIONS; ++i) {
         intersection w = trace(ro, rd, 0.);
 
-        if (w.path < 0.) break;
-        if (w.path > MAX_PATH) {
-//            if (i == 0.) gl_FragColor = vec4(1., 0., 0., 1.);
+        if (w.path < 0.) {
+            if (i == 0.) gl_FragColor = vec4(1., 0., 0., 1.);
             break;
-        };
+        }
+
+        if (w.path > MAX_PATH) {
+            if (i == 0.) gl_FragColor = bg;
+            break;
+        }
 
         vec3 v = ro + rd*w.path;
         vec3 normal = getNormal(v);
-        vec3 lightPos = vec3(0., 10., 0.);
+        vec3 lightPos = vec3(0., 20., 20.);
         vec3 lightDir = lightPos - v;
         vec4 dirLight = getDirLight(v, normal, lightPos);
 
@@ -236,17 +243,20 @@ void main() {
 
         float reflectivity = (REFLECTIONS - i) / REFLECTIONS;
         vec4 color = getMaterial(w, dirLight);
-        gl_FragColor += /*reflectivity **/ /*w.opacity **/ color;
+        gl_FragColor += reflectivity * /*w.opacity **/ color;
 
         // fog
         if (i == 0.) {
             gl_FragColor = mix(gl_FragColor, bg, smoothstep(0., MAX_PATH, w.path));
         }
 
-//        break;
+        break;
 
         // reflection
-        rd = normalize(reflect(rd, normal));
-        ro = v + 2.*rd*MIN_PATH;
+//        rd = normalize(reflect(rd, normal));
+//        ro = v + 2.*rd*MIN_PATH;
+
+        rd = normalize(refract(rd, normal, .5));
+//        ro = o - uv.x*x*ratio + uv.y*y;
     }
 }
